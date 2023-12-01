b27f6bc265fcd4e244a09a5a3f8c98820c84121b
Sandra <sandramcmp@gmail.com>
2023-12-01 16:12:31 +0100

    Añadido gestión de balas. De momento todas matan igual hasta que averigue como hacer que se destruyan objetos en un área

--- Game/Scripts/Game.js
+++ Game/Scripts/Game.js
@@ -8,6 +8,7 @@ class Game extends Phaser.Scene
     preload()
     {
         this.load.image("Character", "./Sprites/Character.png"); //Va estupendo!
+        this.load.image("bullet", "./Sprites/bala.png");
         this.load.spritesheet("placeholder", "./Sprites/SpriteSheet.png", {frameWidth: 172, frameHeight: 269})
     }
 
@@ -22,12 +23,14 @@ class Game extends Phaser.Scene
         this.InitializePlayer(); //El jugador también tendrá una posición aleatoria
         this.InitializePlayer2();
         this.player.ManageInput(this); //Se añade la gestión del input al ser pulsada una tecla. Se pasa como parámetro la escena del juego.
+        this.player2.ManageBullets(); //Se añade la gestión de las balas. Cada vez que se haga click izquierdo, se pierde una bala
         this.player.StopMovement(this); //Gestión del input: cuando deja de pulsarse la tecla de movimiento el jugador se queda quieto
     }
 
     update(time, deltaTime)
     {
         this.UpdateCharacters();
+        this.player2.CheckLose();
     }
 
     InitializeNPCS() //Inicializa todos los NPCs en posiciones aleatorias
@@ -50,9 +53,11 @@ class Game extends Phaser.Scene
     }
 
     InitializePlayer2() {
-        this.player2 = new Player2(this); // Le paso la escena actual. No pongo num balas todavia
-        this.input.on('pointermove',this.player2.UpdatePositionP2.bind(this.player2), this); //Cada vez que el ratón se mueve le paso la función para cambiar la posición del jugador 2 (que va a ser la del ratón)
+       this.player2 = new Player2(this, "F", "bullet"); // Le paso la escena actual. De momento le paso directamente el arma yo, pero después será una variable que vendrá dada por la escena de personalización
+       console.log(this.player2.weapon)
+       this.input.on('pointermove',this.player2.UpdatePositionP2.bind(this.player2), this); //Cada vez que el ratón se mueve le paso la función para cambiar la posición del jugador 2 (que va a ser la del ratón)
        //le paso el contexto con el último this para que lo haga bien
+       this.player2.InitializeBullets();  //Inicializa las balas del jugador según su ar,a
     }
 
     UpdateCharacters() //Actualiza posiciones de los jugadores y NPCs
--- Game/Scripts/Player2.js
+++ Game/Scripts/Player2.js
@@ -1,11 +1,11 @@
 class Player2 extends Phaser.GameObjects.Group
 {
-    constructor(scene){
+    constructor(scene, weapon, weaponimg){
         super({key: "Player2"});
         this.scene = scene
-        //this.bullets = 0;
-        //Meter el arma en constructor
-        //this.arma = armaElegida
+        this.bullets;
+        this.weaponimg = weaponimg;
+        this.weapon = weapon
         this.p2x = 0;
         this.p2y = 0;
         this.gunsight;
@@ -13,13 +13,42 @@ class Player2 extends Phaser.GameObjects.Group
     }
 
     //Hacer función que gestione que dependiendo del tipo de bala tenga X balas
-    //Hacer función que gestione que si tiene menos de las balas que vienen dada por el arma (que vaya comprobando el game) mate al pj dos
-    
+    InitializeBullets(){
+        if(this.weapon == "LG"){
+            this.bullets = new Array(1);
+            
+        } else if(this.weapon == "F"){ 
+            this.bullets = new Array(3);
+        }
+        console.log(this.bullets.length)
+
+        for(var i = 0; i<this.bullets.length; i++){
+            this.bullets[i] = this.scene.add.sprite(1434 + (i*20),456, this.weaponimg) //Aparecen las imágenes una tras otra siguiendo una distancia x+20
+        }
+    }
+    //Si tiene 0 balas se duerme la escena. Luego meter condición de que sean 0 balas y que el jugador1 siga vivo
+    CheckLose(){
+        if(this.bullets.length == 0){ 
+            this.scene.scene.sleep();
+        }
+    }
 
     UpdatePositionP2(pointer) //Se le pasa directamente el puntero activo
     { 
         this.p2x = pointer.x;
         this.p2y = pointer.y;
         //console.log(this.p2x,this.p2y);
+    }
+
+    ManageBullets(){ //Función que hace que se elimine una bala cada vez que se pulsa el botón izquierdo del ratón
+        this.scene.input.on('pointerdown', function(pointer){
+        if(pointer.leftButtonDown()){
+        if(this.bullets.length > 0){
+           const bullet = this.bullets.pop() //Elige la última bala del array 
+           bullet.destroy(); //destruye su sprite
+        }
+        console.log(this.bullets.length)
+        }
+    }.bind(this));
     }
 }
